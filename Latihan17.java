b4f17d71b7974429a6ed9aab6c6ba3c47f7a3237
bintangnusantara388 <145913395+bintangnusantara388@users.noreply.github.com>
2023-10-26 12:38:16 +0700

    Add files via upload

---
+++ Latihan17.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan17;
+import java.util.Scanner;
+
+public class Latihan17 {
+
+    public static void main(String[] args) {
+           int gajipokok;
+        String status;
+        double tunjangan;
+        
+        System.out.println("======Program Tunjangan======");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Berapa gaji pokok anda perbulan? :");
+        gajipokok = scanner.nextInt();
+        System.out.print("Status Anda? (menikah/belum) :");
+        status = scanner.next();
+        System.out.println("==============================");
+               
+        if (status.equalsIgnoreCase("Menikah")) {
+            tunjangan = gajipokok * 0.35;
+        } else 
+            tunjangan = 0;
+
+        System.out.println("\n");
+        System.out.println("======Hasil Perhitungan Gaji Anda======");
+        System.out.println("Gaji Pokok: Rp " + gajipokok);
+        System.out.println("Tunjangan: Rp " + tunjangan);
+        System.out.println("Total Gaji: Rp " + (gajipokok + tunjangan));
+        System.out.println("(Developed by : Bintang Fajar Nusantara)");
+        
+    }
+    
+}
